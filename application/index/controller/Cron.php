dece93db889879a29b5395e471759947c9bc8af2
gin <2209269332@qq.com>
2018-04-08 16:24:30 +0800

    Changes to be committed: 	new file:   application/index/controller/Cron.php

---
+++ application/index/controller/Cron.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+use think\Db;
+use think\Request;
+    
+class Corn extends Controller{
+
+    public function hour(){
+        error_log1('log1.txt',date('Y-m-d H:i:s',$_SERVER['REQUEST_TIME']));
+    }
+}
