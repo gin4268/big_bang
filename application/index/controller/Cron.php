d63ae73f97b308c1c5a566b00dd0636008a8a2ef
gin <2209269332@qq.com>
2018-04-08 16:27:04 +0800

    Changes to be committed: 	modified:   application/index/controller/Cron.php

--- application/index/controller/Cron.php
+++ application/index/controller/Cron.php
@@ -4,7 +4,7 @@ use think\Controller;
 use think\Db;
 use think\Request;
     
-class Corn extends Controller{
+class Cron extends Controller{
 
     public function hour(){
         error_log1('log1.txt',date('Y-m-d H:i:s',$_SERVER['REQUEST_TIME']));
