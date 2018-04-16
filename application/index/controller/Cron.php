8809fe8c9e2123690d56af159826aa6c693490cb
gin <2209269332@qq.com>
2018-04-16 13:55:11 +0800

    修改定时任务文件,修改登录session保存时间  Changes to be committed: 	modified:   application/config.php 	modified:   application/index/controller/Cron.php

--- application/index/controller/Cron.php
+++ application/index/controller/Cron.php
@@ -6,7 +6,18 @@ use think\Request;
     
 class Cron extends Controller{
 
+    // 每小时执行
     public function hour(){
-        error_log1('log1.txt',date('Y-m-d H:i:s',$_SERVER['REQUEST_TIME']));
+        error_log1('log1.txt',date('Y-m-d H:i:s',$_SERVER['REQUEST_TIME']).'  hour');
+    }
+
+    // 每分钟执行
+    public function min(){
+        error_log1('log1.txt',date('Y-m-d H:i:s',$_SERVER['REQUEST_TIME']).'  min');
+    }
+
+    // 每天执行
+    public function day(){
+        error_log1('log1.txt',date('Y-m-d H:i:s',$_SERVER['REQUEST_TIME']).'  day');
     }
 }
