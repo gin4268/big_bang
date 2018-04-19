be0b2135cd1ae062484dbfd651a03a2ce05c4e2e
gin <2209269332@qq.com>
2018-04-19 15:25:56 +0800

    修改定时任务  Changes to be committed: 	modified:   application/index/controller/Cron.php

--- application/index/controller/Cron.php
+++ application/index/controller/Cron.php
@@ -8,16 +8,16 @@ class Cron extends Controller{
 
     // 每小时执行
     public function hour(){
-        error_log1('log1.txt',date('Y-m-d H:i:s',$_SERVER['REQUEST_TIME']).'  hour');
+        // error_log1('log1.txt',date('Y-m-d H:i:s',$_SERVER['REQUEST_TIME']).'  hour');
     }
 
     // 每分钟执行
     public function min(){
-        error_log1('log1.txt',date('Y-m-d H:i:s',$_SERVER['REQUEST_TIME']).'  min');
+        // error_log1('log1.txt',date('Y-m-d H:i:s',$_SERVER['REQUEST_TIME']).'  min');
     }
 
     // 每天执行
     public function day(){
-        error_log1('log1.txt',date('Y-m-d H:i:s',$_SERVER['REQUEST_TIME']).'  day');
+        // error_log1('log1.txt',date('Y-m-d H:i:s',$_SERVER['REQUEST_TIME']).'  day');
     }
 }
